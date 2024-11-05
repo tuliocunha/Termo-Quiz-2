d22399adef8c5e69bc705ece041541caa332821e
tuliocunha <tulio.cunha@hotmail.com>
2024-11-05 00:39:34 +0000

    Delete js directory

--- js/js/quiz_selection.js
+++
@@ -1,5 +0,0 @@
-// quiz_selection.js
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Quiz Selection Page Loaded');
-    // Add your quiz selection logic here
-});
--- js/quiz.js
+++
@@ -1,5 +0,0 @@
-// quiz.js
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Quiz Page Loaded');
-    // Add your quiz logic here
-});
